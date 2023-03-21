a314cb24e31b4621005177509cc5cd0a9008db49
Léa LUTTENBACHER <lea.luttenbacher@edu.devinci.fr>
2023-03-21 18:42:53 +0100

    feat(new-products): insert all products

--- server/Connect_Mongodb.js
+++ server/Connect_Mongodb.js
@@ -10,10 +10,6 @@ async function connectToDatabase() {
 
     const db = client.db(MONGODB_DB_NAME);
     console.log('Connected to MongoDB database:', MONGODB_DB_NAME);
-
-    // Any additional code that needs to interact with the database can be added here
-
-    // Don't forget to close the connection when you're done!
     await client.close();
     console.log('Disconnected from MongoDB database:', MONGODB_DB_NAME);
 }
@@ -53,7 +49,85 @@ async function main() {
     }
 }
 
-main();
+const brand = 'Montlimart';
+
+async function findProductsByBrand(db, brand) {
+    const collection = db.collection('products');
+    const products = await collection.find({ brand }).toArray();
+    console.log(`Found ${products.length} products for brand '${brand}'`);
+    console.log(products);
+}
+
+async function query1() {
+    const client = await MongoClient.connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    const db = client.db(MONGODB_DB_NAME);
+
+    try {
+        await findProductsByBrand(db, brand);
+    } catch (error) {
+        console.error(error);
+    } finally {
+        await client.close();
+    }
+}
+
+async function findProductsByPriceLessThan(db, price) {
+    const collection = db.collection('products');
+    const products = await collection.find({ price: { $lt: price } }).toArray();
+    console.log(`Found ${products.length} products with price less than ${price}`);
+    console.log(products);
+}
+
+
+async function query2() {
+    const client = await MongoClient.connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    const db = client.db(MONGODB_DB_NAME);
+
+    try {
+        await findProductsByPriceLessThan(db, 20);
+    } catch (error) {
+        console.error(error);
+    } finally {
+        await client.close();
+    }
+}
+
+async function findProductsSortedByPrice(db, sortDirection = 1) {
+    const collection = db.collection('products');
+    const sort = { price: sortDirection };
+    const products = await collection.find().sort(sort).toArray();
+    console.log(`Found ${products.length} products sorted by price (in ${sortDirection === 1 ? 'ascending' : 'descending'} order)`);
+    console.log(products);
+}
+
+async function query3() {
+    const client = await MongoClient.connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    const db = client.db(MONGODB_DB_NAME);
+
+    try {
+        await findProductsSortedByPrice(db,-1); //-1 to sort the result in descending order
+    } catch (error) {
+        console.error(error);
+    } finally {
+        await client.close();
+    }
+}
+
+query1()
+query2()
+query3()
+
+
+
 
 
 
